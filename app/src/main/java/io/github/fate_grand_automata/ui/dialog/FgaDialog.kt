67e0f134736769c2458bdf95979ec43c8af9baeb
GYC <guyuchao3@vip.qq.com>
2025-10-01 01:16:57 +0800

    Revert "Add fillMaxHeight to temporarily fix the problem with being able to c…"

--- app/src/main/java/io/github/fate_grand_automata/ui/dialog/FgaDialog.kt
+++ app/src/main/java/io/github/fate_grand_automata/ui/dialog/FgaDialog.kt
@@ -281,7 +281,6 @@ fun <T> FgaDialog.singleChoiceList(
             contentPadding = PaddingValues(16.dp, 0.dp),
             modifier = modifier
                 .fillMaxWidth()
-                .fillMaxHeight()
         ) {
             items(items) {
                 ChoiceListItem(
